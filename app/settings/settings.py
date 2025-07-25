e34fc5c59898c2003201f4cd24d05a11175ea22a
Evgeniy <myiphone128@mail.ru>
2025-07-25 10:26:49 +0300

    fixes

--- app/settings/settings.py
+++ app/settings/settings.py
@@ -27,9 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-# CORS_ALLOWED_ORIGINS = [
-#     "*",
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://localhost",
+    "http://kart-podo.eu"
+]
 
 CORS_ALLOW_ALL_ORIGINS = True
 
